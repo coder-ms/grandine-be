8729a935bb007d4cfae7eee518582ae5cd8c5b0c
coder-ms <marco.sbrascia@gmail.com>
2023-03-27 12:46:42 +0200

    practiceseeder

--- config/practices.php
+++ config/practices.php
@@ -2,25 +2,25 @@
 return [
     "practices" => [
         [
-            "plate" => "gb925BB",
+            "plate" => "GB925BB",
             "model" => "Tesla Model 3",
             "color" => "Grigio metallizzato",
             "immatricolation" => 2021,
             "cover" => "img/cinese.jpg"
         ],
         [
-            "plate" => "bacon burger",
-            "price" => "7.10",
-            "visible" => 1,
-            "ingredients" => "pane, bacon, hamburger, cheddar, maionese, ketchup",
-            "image" => "img/bacon burger.png",
+            "plate" => "CC376UC",
+            "model" => "Fiat Punto",
+            "color" => "Giallo Limone",
+            "immatricolation" => 2002,
+            "cover" => "img/cinese.jpg"
         ],
         [
-            "plate" => "margherita",
-            "price" => "5.50",
-            "visible" => 1,
-            "ingredients" => "salsa di pomodoro, mozzarella, basilico",
-            "image" => "img/margherita.png",
+            "plate" => "EL219KK",
+            "model" => "Alfa Romeo Giulietta",
+            "color" => "Rosso",
+            "immatricolation" => 2014,
+            "cover" => "img/cinese.jpg"
         ],
     ]
 ];
